af1f609f6c14557e70c93fd8126d6414f13c302e
KaO <52933590+painteau@users.noreply.github.com>
2024-08-27 18:47:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,10 @@ def after_request(response):
     return response
 
 # MQTT setup
-mqtt_client = mqtt.Client()
+mqtt_client = mqtt.Client(client_id='mqttui',
+                         transport='tcp',
+                         protocol=mqtt.MQTTv311,
+                         clean_session=True)
 mqtt_broker = os.getenv('MQTT_BROKER', 'localhost')
 mqtt_port = int(os.getenv('MQTT_PORT', 1883))
 mqtt_username = os.getenv('MQTT_USERNAME')
